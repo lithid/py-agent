c3360789f706f024e4f27877e980107521a4fcc3
lithid <long.jeremie@gmail.com>
2025-06-18 07:31:26 -0400

    Add other fuction schema

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,6 @@
 import os
 from google.genai import types
+
 from config import MAX_CHARS
 
 
@@ -22,16 +23,17 @@ def get_file_content(working_directory, file_path):
         return f'Error reading file "{file_path}": {e}'
 
 
-schema_get_files_info = types.FunctionDeclaration(
-    name="get_files_info",
-    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description=f"Reads and returns the first {MAX_CHARS} characters of the content from a specified file within the working directory.",
     parameters=types.Schema(
         type=types.Type.OBJECT,
         properties={
-            "directory": types.Schema(
+            "file_path": types.Schema(
                 type=types.Type.STRING,
-                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+                description="The path to the file whose content should be read, relative to the working directory.",
             ),
         },
+        required=["file_path"],
     ),
 )
